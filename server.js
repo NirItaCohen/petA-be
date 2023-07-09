4f1f2fbad08f6629e3c1503abc0c307d06bce1a1
Nir Cohen <nir.cohen.a30@gmail.com>
2023-07-09 12:36:22 +0300

    updating repo

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const DB = process.env.DATABASE.replace(
   "<PASSWORD>",
   process.env.DATABASE_PASSWORD
 );
+
 mongoose.connect(DB).then((con) => {
   console.log("DB connection successful!");
 });
